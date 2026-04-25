cfd1292eeef0f6e24ae0c08153adb3948b23bc91
goslynn <vct.gonzaleza@gmail.com>
2026-04-24 20:19:45 -0400

    innit quarkus

---
+++ src/test/java/cl/duocuc/edutrack/ms/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package cl.duocuc.edutrack.ms;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
